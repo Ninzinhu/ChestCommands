8b62501c7bacef330544fb257c27d4c2ec97d3b3
Joao Vasconcelos <pel118201@gmail.com>
2026-01-31 18:36:58 -0300

    refactor: remover módulos antigos da estrutura multi-módulo

--- chestcommands-api/src/main/java/org/konpekiestudios/chestcommands/api/ChestCommandsAPI.java
+++
@@ -1,9 +0,0 @@
-package org.konpekiestudios.chestcommands.api;
-
-import java.util.function.Function;
-
-public interface ChestCommandsAPI {
-    void openMenu(PlayerRef player, String menuId);
-    void registerAction(String id, Function<ActionContext, Action> factory);
-    void registerCondition(String id, Function<ActionContext, Condition> factory);
-}
